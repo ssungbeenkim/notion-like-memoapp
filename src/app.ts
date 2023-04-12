60bc5d58f196a20bd6a16fddd74a6f991789fd84
wandakim <nwd1583@naver.com>
2023-04-12 16:46:08 +0900

    Page Item Component

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,32 @@
-import { PageComponent } from './components/page/page.js';
+import { Composable, PageComponent } from './components/page/page.js';
+import { Component } from './components/Component.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { NoteComponent } from './components/page/item/note.js';
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
-    this.page = new PageComponent();
+    this.page = new PageComponent(); // dependency injection이 낫기는 하다.
     this.page.attachTo(appRoot);
+
     const image = new ImageComponent(
       'Image Title',
       'http://picsum.photos/600/300'
     );
-    image.attachTo(appRoot, 'beforeend');
+    this.page.addChild(image);
 
     const note = new NoteComponent('Note Title', 'Note Body');
-    note.attachTo(appRoot, 'beforeend');
+    this.page.addChild(note);
 
     const todo = new TodoComponent('Todo Title', 'Todo Item');
-    todo.attachTo(appRoot, 'beforeend');
+    this.page.addChild(todo);
 
     const video = new VideoComponent(
       'Video Title',
       'https://youtu.be/XIOoqJyx8E4'
     );
-    video.attachTo(appRoot, 'beforeend');
+    this.page.addChild(video);
   }
 }
 new App(document.querySelector('.document')! as HTMLElement);
