d6dd96396caa556266e27561f487995087645592
wandakim <nwd1583@naver.com>
2023-04-14 17:20:20 +0900

    Handle delete button:no DI

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,10 +4,12 @@ export interface Composable {
   addChild(child: Component): void;
 }
 
-class PageItemComponent
+type OnCloseListener = () => void;
+class PageItemComponent // container를 만든다. section을 받아서 만든 container에 넣는다.
   extends BaseComponent<HTMLElement>
   implements Composable
 {
+  private closeListener?: OnCloseListener;
   constructor() {
     super(`<li class="page-item">
             <section class="page-item__body"></section>
@@ -15,6 +17,10 @@ class PageItemComponent
               <button class="close">&times;</button>
             </div>
           </li>`);
+    const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
   }
 
   addChild(child: Component) {
@@ -23,7 +29,14 @@ class PageItemComponent
     )! as HTMLElement;
     child.attachTo(container);
   }
+
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
+  }
 }
+
+// page 요소를 만든다.
+// addChild: 아이템 컴포넌트를 이용해 아이템 요소를 만들고 섹션을 전달해 아이템에 추가하고 아이템을 페이지에 추가한다.
 export class PageComponent
   extends BaseComponent<HTMLUListElement>
   implements Composable
@@ -36,5 +49,8 @@ export class PageComponent
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, 'beforeend');
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });
   }
 }
