ad5143de6f1f08e3f47461ef2ea8cdad0d699470
wandakim <nwd1583@naver.com>
2023-05-04 15:24:53 +0900

    review

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 /* 랜덤 이미지 https://picsum.photos/600/300 */
-// 유튜브
 import {
   Composable,
   PageComponent,
@@ -52,12 +51,15 @@ class App {
       (input: TextSectionInput) => new TodoComponent(input.title, input.body)
     );
 
-    // For demo :)
+    // For demo
     this.page.addChild(
       new ImageComponent('Image Title', 'https://picsum.photos/800/400')
     );
     this.page.addChild(
-      new VideoComponent('Video Title', '')
+      new VideoComponent(
+        'Video Title',
+        'https://www.youtube.com/watch?v=XIOoqJyx8E4'
+      )
     );
     this.page.addChild(
       new NoteComponent('Note Title', "Don't forget to code your dream")
@@ -67,7 +69,10 @@ class App {
       new ImageComponent('Image Title', 'https://picsum.photos/800/400')
     );
     this.page.addChild(
-      new VideoComponent('Video Title', 'https://youtu.be/D7cwvvA7cP0')
+      new VideoComponent(
+        'Video Title',
+        'https://www.youtube.com/watch?v=XIOoqJyx8E4'
+      )
     );
     this.page.addChild(
       new NoteComponent('Note Title', "Don't forget to code your dream")
@@ -86,7 +91,6 @@ class App {
       const input = new InputComponent();
       dialog.addChild(input);
       dialog.attachTo(this.dialogRoot);
-
       dialog.setOnCloseListener(() => {
         dialog.removeFrom(this.dialogRoot);
       });
