08beb12ec9c94b15dcfed972ad5a3c445cced807
wandakim <nwd1583@naver.com>
2023-05-05 15:02:41 +0900

    Drag event

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -20,7 +20,7 @@ export class PageItemComponent
 {
   private closeListener?: OnCloseListener;
   constructor() {
-    super(`<li class="page-item">
+    super(`<li draggable="true" class="page-item">
             <section class="page-item__body"></section>
             <div class="page-item__controls">
               <button class="close">&times;</button>
@@ -30,6 +30,20 @@ export class PageItemComponent
     closeBtn.onclick = () => {
       this.closeListener && this.closeListener();
     };
+    this.element.addEventListener('dragstart', (event: DragEvent) => {
+      this.onDragStart(event);
+    });
+    this.element.addEventListener('dragend', (event: DragEvent) => {
+      this.onDragEnd(event);
+    });
+  }
+
+  onDragStart(event: DragEvent) {
+    console.log('dragstart', event);
+  }
+
+  onDragEnd(event: DragEvent) {
+    console.log('dragend', event);
   }
 
   addChild(child: Component) {
@@ -50,6 +64,22 @@ export class PageComponent
 {
   constructor(private pageItemConstructor: SectionContainerConstructor) {
     super('<ul class="page"></ul>');
+    this.element.addEventListener('dragover', (event: DragEvent) => {
+      this.onDragOver(event);
+    });
+    this.element.addEventListener('drop', (event: DragEvent) => {
+      this.onDrop(event);
+    });
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    console.log('onDragOver', event);
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    console.log('onDrop', event);
   }
 
   addChild(section: Component) {
